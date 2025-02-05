9f3d1ffa2212f352e51218d258606401f429e1f5
selva001 <siva.selvanayagam@wur.nl>
2025-02-05 11:32:01 +0100

    Added new scoring method based on the weights

--- src/main/java/nl/wur/bis/kcftools/Data/Data.java
+++ src/main/java/nl/wur/bis/kcftools/Data/Data.java
@@ -1,32 +1,45 @@
 package nl.wur.bis.kcftools.Data;
 
 public class Data{
-    int kmerDistance;
+    int tailDistance;
+    int innerDistance;
     int observedKmers;
     int variations;
     float score;
     int ibs;
 
-    public Data(int observedKmers, int variations, int kmerDistance, int totalKmers){
+    public Data(int observedKmers, int variations, int innerDistance, int tailDistance, int totalKmers, int effLength) {
         this.observedKmers = observedKmers;
         this.variations = variations;
-        this.kmerDistance = kmerDistance;
-        this.score = computeScore(totalKmers);
+        this.innerDistance = innerDistance;
+        this.tailDistance = tailDistance;
+        this.score = computeScore(totalKmers, effLength);
         this.ibs = -1;
     }
 
-    public Data() {
-        this.ibs = -1;
+    public Data(int observedKmers, int variations, int innerDistance, int tailDistance, int totalKmers, int effLength, String ibs) {
+        this.observedKmers = observedKmers;
+        this.variations = variations;
+        this.innerDistance = innerDistance;
+        this.tailDistance = tailDistance;
+        this.score = computeScore(totalKmers, effLength);
+        if (ibs.equals("N")){
+            this.ibs = -1;
+        } else {
+            this.ibs = Integer.parseInt(ibs);
+        }
     }
 
-    public float computeScore(int totalKmers){
-        if ((observedKmers == 0) || (totalKmers == 0)){
+    public float computeScore(int totalKmers, int effLength){
+        if ((observedKmers == 0) || (totalKmers == 0) || (effLength == 0)){
             return 0;
         }
 //        return (float) (observedKmers - variations) * 100 / totalKmers;
 //        var$score <- (var$observed_kmers / var$total_kmers) * (1 - (var$kmer_distance / ((var$end-var$start+1)))) * 100
-        return (float) observedKmers / totalKmers * (1 - (float) kmerDistance / (float) (totalKmers)) * 100;
-
+//        return (float) observedKmers / totalKmers * (1 - (float) kmerDistance / (float) (totalKmers)) * 100;
+        return ((0.4f * observedKmers / totalKmers)
+                + (0.4f * (1.0f - ((float) innerDistance / effLength)))
+                + (0.2f * (1.0f - ((float) tailDistance / effLength)))) * 100.0f;
     }
 
     public float getScore(){
@@ -35,10 +48,11 @@ public class Data{
 
     @Override
     public String toString(){
+        // ibs:variations:observedKmers:innerDistance:tailDistance:score
         if (ibs == -1){
-            return "N" + ":" + variations + ":" + observedKmers + ":" + kmerDistance + ":" + String.format("%.2f", score);
+            return "N" + ":" + variations + ":" + observedKmers + ":" + innerDistance + ":" + tailDistance + ":" + String.format("%.2f", score);
         }
-        return ibs + ":" + variations + ":" + observedKmers + ":" + kmerDistance + ":" + String.format("%.2f", score);
+        return ibs + ":" + variations + ":" + observedKmers + ":" + innerDistance + ":" + tailDistance + ":" + String.format("%.2f", score);
     }
 
     public void setIBS(int ibs) {
@@ -58,7 +72,7 @@ public class Data{
     }
 
     public String toTSV() {
-        return observedKmers + "\t" + variations + "\t" + kmerDistance;
+        return observedKmers + "\t" + variations + "\t" + innerDistance;
     }
 }
 // EOF
