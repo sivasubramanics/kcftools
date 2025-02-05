aed20b0e9e49ed1bc8557fb3fad83ad6740cb964
selva001 <siva.selvanayagam@wur.nl>
2025-02-05 11:36:25 +0100

    refactor of config enum

--- src/main/java/nl/wur/bis/kcftools/Utils/Configs.java
+++ src/main/java/nl/wur/bis/kcftools/Utils/Configs.java
@@ -5,22 +5,25 @@ public enum Configs {
     KCF_SOURCE("kcftools"),
     KCF_DATE(HelperFunctions.getTodayDate()),
     KCF_INFO_LINES(
-            "<ID=IS,Type=Float,Description=\"Minimum score for the window\">\n" +
-            "<ID=XS,Type=Float,Description=\"Maximum score for the window\">\n" +
-            "<ID=MS,Type=Float,Description=\"Mean score for the window\">\n" +
-            "<ID=IO,Type=Integer,Description=\"Minimum observed kmers in the window\">\n" +
-            "<ID=XO,Type=Integer,Description=\"Maximum observed kmers in the window\">\n" +
-            "<ID=MO,Type=Integer,Description=\"Mean observed kmers in the window\">\n" +
-            "<ID=IV,Type=Integer,Description=\"Minimum variations in the window\">\n" +
-            "<ID=XV,Type=Integer,Description=\"Maximum variations in the window\">\n" +
-            "<ID=MV,Type=Integer,Description=\"Mean variations in the window\">"
+            """
+                    <ID=EFFLEN,Type=Integer,Description="Effective length of the window">
+                    <ID=IS,Type=Float,Description="Minimum score for the window">
+                    <ID=XS,Type=Float,Description="Maximum score for the window">
+                    <ID=MS,Type=Float,Description="Mean score for the window">
+                    <ID=IO,Type=Integer,Description="Minimum observed kmers in the window">
+                    <ID=XO,Type=Integer,Description="Maximum observed kmers in the window">
+                    <ID=MO,Type=Integer,Description="Mean observed kmers in the window">
+                    <ID=IV,Type=Integer,Description="Minimum variations in the window">
+                    <ID=XV,Type=Integer,Description="Maximum variations in the window">
+                    <ID=MV,Type=Integer,Description="Mean variations in the window">"""
     ),
     KCF_FORMAT_LINES(
-            "<ID=IB,Type=Integer,Description=\"IBS number\">\n" +
-            "<ID=VA,Type=Integer,Description=\"Variations\">\n" +
-            "<ID=OB,Type=Integer,Description=\"Observed kmers\">\n" +
-            "<ID=DI,Type=Integer,Description=\"Kmer Variation Distance\">\n" +
-            "<ID=SC,Type=Float,Description=\"Score\">"
+            """
+                    <ID=IB,Type=Integer,Description="IBS number">
+                    <ID=VA,Type=Integer,Description="Variations">
+                    <ID=OB,Type=Integer,Description="Observed kmers">
+                    <ID=DI,Type=Integer,Description="Kmer Variation Distance">
+                    <ID=SC,Type=Float,Description="Score">"""
     );
 
     private final String value;
